fc95bc4b33f688aa6a9446d1e13f83524cca4995
Kian Nejadfard <k.nejadfard@gmail.com>
2015-07-23 19:58:35 -0400

    Added constants header

---
+++ src/include/constants.h
@@ -0,0 +1,7 @@
+/**************************************
+ * Size constants used by the program *
+ *************************************/
+const int buffer = 512;
+const int kb = 1024;
+const int mb = 1048576;
+const int gb = 1073741824;
