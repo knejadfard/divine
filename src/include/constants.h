1fc6812a7b0b6723731ca9c4b5ff81e5163a219e
Kian Nejadfard <k.nejadfard@gmail.com>
2015-07-24 15:21:34 -0400

    Added header guards

--- src/include/constants.h
+++ src/include/constants.h
@@ -1,8 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
 /**************************************
  * Size constants used by the program *
  *************************************/
-const unsigned long buffer = 512;
+const unsigned long buffer_size = 512;
 const unsigned long kb = 1024;
 const unsigned long mb = 1048576;
 const unsigned long gb = 1073741824;
 const unsigned long tb = 1099511627776;
+
+#endif
